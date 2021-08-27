62c9e9d9f7a9eac5da4c695b2c6becf4a2409528
Maniarr <nicolas@maniarr.fr>
2021-08-27 15:24:39 +0200

    feat: twitch & youtube notification

---
+++ discord/src/main.rs
@@ -0,0 +1,204 @@
+use std::env;
+
+use serenity::{
+    async_trait,
+    model::{channel::Message as DiscordMessage, gateway::Ready},
+    prelude::*,
+};
+use serenity::model::id::ChannelId;
+
+use rdkafka::client::ClientContext;
+use rdkafka::config::{ClientConfig, RDKafkaLogLevel};
+use rdkafka::consumer::stream_consumer::StreamConsumer;
+use rdkafka::consumer::{CommitMode, Consumer, ConsumerContext, Rebalance};
+use rdkafka::message::Message;
+
+use serde::{Deserialize, Serialize};
+use serde_json::{
+    json,
+    Value as JsonValue,
+};
+use std::collections::HashMap;
+
+use handlebars::Handlebars;
+
+#[derive(Debug, Deserialize, Serialize)]
+struct EventMessage {
+    #[serde(rename="type")]
+    event_type: String,
+    event: TwitchEvent,
+    triggered_at: chrono::DateTime<chrono::Utc>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(untagged)]
+enum TwitchEvent {
+    StreamOnline {
+        id: String,
+        broadcaster_user_id: String,
+        broadcaster_user_login: String,
+        broadcaster_user_name: String,
+        #[serde(rename="type")]
+        stream_type: String,
+        started_at: chrono::DateTime<chrono::Utc>,
+    },
+    StreamOffline {
+        broadcaster_user_id: String,
+        broadcaster_user_login: String,
+        broadcaster_user_name: String,
+    }
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+struct Author {
+    name: String,
+    uri: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct EventYoutubeVideo {
+    id: String,
+    channel_id: String,
+    title: String,
+    link: String,
+    author: Author,
+    published_at: chrono::DateTime<chrono::Utc>,
+    updated_at: chrono::DateTime<chrono::Utc>
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct KafkaConfiguration {
+    broker_ip: String,
+    topics: Vec<String>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Handler {
+    youtube: HashMap<String, HashMap<u64, String>>,
+    twitch: HashMap<String, HashMap<String, HashMap<u64, String>>>,
+    kafka: KafkaConfiguration,
+}
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: DiscordMessage) {
+        if msg.content == "!ping" {
+            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
+                println!("Error sending message: {:?}", why);
+            }
+        }
+    }
+
+    async fn ready(&self, ctx: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+
+        let consumer: StreamConsumer = ClientConfig::new()
+            .set("group.id", "discord")
+            .set("bootstrap.servers", &self.kafka.broker_ip)
+            .set("enable.partition.eof", "false")
+            .set("session.timeout.ms", "6000")
+            .set("enable.auto.commit", "true")
+            .create()
+            .expect("Consumer creation failed");
+
+        consumer
+            .subscribe(&self.kafka.topics.iter().map(|i| i.as_str()).collect::<Vec<&str>>())
+            .expect("Can't subscribe to specified topics");
+
+        let mut cache: HashMap<String, bool> = HashMap::new();
+
+        loop {
+            match consumer.recv().await {
+                Err(e) => println!("Kafka error: {}", e),
+                Ok(m) => {
+                    let payload = match m.payload_view::<str>() {
+                        None => "",
+                        Some(Ok(s)) => s,
+                        Some(Err(e)) => {
+                            println!("Error while deserializing message payload: {:?}", e);
+                            ""
+                        }
+                    };
+
+                    if m.topic() == "twitch" {
+                        match serde_json::from_str::<EventMessage>(payload) {
+                            Ok(event) => {
+                                match event.event {
+                                    TwitchEvent::StreamOnline { broadcaster_user_id, broadcaster_user_name, started_at, ..} => {
+                                        if let Some(online_mapping) = self.twitch.get(&event.event_type) {
+                                            if let Some(entries) = online_mapping.get(&broadcaster_user_id) {
+                                                let mut handlebars = Handlebars::new();
+                                                handlebars.register_escape_fn(handlebars::no_escape);
+                                                
+                                                for (channel_id, message_format) in entries {
+                                                    ChannelId(channel_id.clone()).say(&ctx.http, handlebars.render_template(message_format, &json!({"broadcaster_name": broadcaster_user_name, "broadcaster_login": broadcaster_user_login})).unwrap()).await;
+                                                }
+                                    
+                                            }
+                                        }
+                                    },
+                                    TwitchEvent::StreamOffline { broadcaster_user_id, broadcaster_user_name, .. } => {
+                                        if let Some(online_mapping) = self.twitch.get(&event.event_type) {
+                                            if let Some(entries) = online_mapping.get(&broadcaster_user_id) {
+                                                let mut handlebars = Handlebars::new();
+                                                handlebars.register_escape_fn(handlebars::no_escape);
+                                                
+                                                for (channel_id, message_format) in entries {
+                                                    ChannelId(channel_id.clone()).say(&ctx.http, handlebars.render_template(message_format, &json!({"broadcaster_name": broadcaster_user_name})).unwrap()).await;
+                                                }
+                                            }
+                                        }
+                                    },
+                                };
+                            },
+                            Err(error) => {
+
+                            }
+                        };
+                    } else {
+                        match serde_json::from_str::<EventYoutubeVideo>(payload) {
+                            Ok(video) => {
+                                if !cache.contains_key(&video.id) {
+                                    if let Some(entries) = self.youtube.get(video.channel_id.as_str()) {
+                                        let mut handlebars = Handlebars::new();
+                                        handlebars.register_escape_fn(handlebars::no_escape);
+                                        
+                                        for (channel_id, message_format) in entries {
+                                            ChannelId(channel_id.clone()).say(&ctx.http, handlebars.render_template(message_format, &json!({"video_link": video.link})).unwrap()).await;
+                                        }
+
+                                        cache.insert(video.id.clone(),true);
+                                    } else {
+                                        println!("Channel id not found in youtube mapping {}", &video.channel_id);
+                                    }
+                                } else {
+                                    println!("Video in cache, skip the message");
+                                }
+                            },
+                            Err(error) => {
+
+                            }
+                        }
+                    }
+
+                    consumer.commit_message(&m, CommitMode::Async).unwrap();
+                }
+            };
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+
+    let config_content = std::fs::read_to_string("config.yml").expect("Failed to read config.yml");
+
+    let handler: Handler = serde_yaml::from_str(&config_content).expect("Not valid yaml inconfig.yml");
+
+    let mut client = Client::builder(&token).event_handler(handler).await.expect("Err creating client");
+    
+    if let Err(why) = client.start().await {
+        println!("Client error: {:?}", why);
+    }
+}
---
+++ webhook/src/main.rs
@@ -0,0 +1,290 @@
+use actix_web::{post, get, web, App, HttpServer, Responder, HttpRequest, web::Bytes, HttpResponse, http::HeaderMap};
+
+use serde_json::{
+    self,
+    json,
+    Value as JsonValue,
+};
+use serde::{Deserialize, Serialize};
+use sha2::Sha256;
+use hmac::{Hmac, Mac, NewMac};
+
+use std::sync::Arc;
+
+use std::time::Duration;
+use rdkafka::{
+    config::ClientConfig,
+    producer::{FutureProducer, FutureRecord},
+};
+
+use std::env;
+
+use actix_web::{middleware::Logger};
+
+#[derive(Debug, Deserialize)]
+struct TwitchTransport {
+    method: String,
+    callback: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct TwitchSubscription {
+    id: String,
+    #[serde(rename="type")]
+    event_type: String,
+    version: String,
+    status: String,
+    cost: i64,
+    condition: JsonValue,
+    transport: TwitchTransport,
+    created_at: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct TwitchCallback {
+    challenge: Option<String>,
+    subscription: TwitchSubscription,
+    event: Option<JsonValue>,
+}
+
+#[derive(Debug, Serialize)]
+struct EventMessage {
+    #[serde(rename="type")]
+    event_type: String,
+    event: JsonValue,
+    triggered_at: chrono::DateTime<chrono::Utc>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct EventYoutubeVideo {
+    id: String,
+    channel_id: String,
+    title: String,
+    link: String,
+    author: Author,
+    published_at: chrono::DateTime<chrono::Utc>,
+    updated_at: chrono::DateTime<chrono::Utc>
+}
+
+impl From<&YoutubeVideo> for EventYoutubeVideo {
+    fn from(video: &YoutubeVideo) -> Self {
+        Self {
+            id: video.id.clone(),
+            channel_id: video.channel_id.clone(),
+            title: video.title.clone(),
+            link: video.link.href.clone(),
+            author: video.author.clone(),
+            published_at: video.published_at,
+            updated_at: video.updated_at,
+        }
+    }
+}
+
+#[derive(Debug, Deserialize)]
+struct YoutubeQuery {
+    #[serde(rename="hub.challenge")]
+    challenge: String,
+    #[serde(rename="hub.lease_seconds")]
+    lease_seconds: i64,
+    #[serde(rename="hub.mode")]
+    mode: String,
+    #[serde(rename="hub.topic")]
+    topic: String,
+    #[serde(rename="hub.verify_token")]
+    verify_token: Option<String>
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+struct Author {
+    name: String,
+    uri: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct Link {
+    href: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct YoutubeVideo {
+    #[serde(rename="videoId")]
+    id: String,
+    #[serde(rename="channelId")]
+    channel_id: String,
+    title: String,
+    link: Link,
+    #[serde(rename="author")]
+    author: Author,
+    #[serde(rename="published")]
+    published_at:  chrono::DateTime<chrono::Utc>,
+    #[serde(rename="updated")]
+    updated_at:  chrono::DateTime<chrono::Utc>
+}
+
+#[derive(Debug, Deserialize)]
+struct YoutubeFeed {
+    #[serde(rename = "entry", default)]
+    entries: Vec<YoutubeVideo>,
+}
+
+fn verify_twitch_signature(headers: &HeaderMap, body: &Bytes, secret: &str) -> bool {
+    let mut mac = Hmac::<Sha256>::new_from_slice(secret.as_bytes())
+        .expect("HMAC can take key of any size");
+
+    if let (Some(id), Some(timestamp), Some(req_signature)) = (headers.get("Twitch-Eventsub-Message-Id"), headers.get("Twitch-Eventsub-Message-Timestamp"), headers.get("Twitch-Eventsub-Message-Signature")) {
+        mac.update(id.as_bytes());
+        mac.update(timestamp.as_bytes());
+        mac.update(&body);
+        
+        return req_signature.to_str().unwrap() == format!("sha256={}", hex::encode(mac.finalize().into_bytes()));
+    }
+
+    false
+}
+
+fn verify_youtube_signature(headers: &HeaderMap, body: &Bytes, secret: &[u8]) -> bool {
+    if let Some(req_signature) = headers.get("X-Hub-Signature") {
+        return 
+            req_signature.to_str().unwrap() == 
+            format!("sha1={}", hex::encode(hmacsha1::hmac_sha1(secret, &body)));
+    }
+
+    false
+}
+
+#[get("/json")]
+async fn test() -> impl Responder {
+    return HttpResponse::Ok()
+        .header("Bloom-Response-Buckets", "page:donations")
+        .json(json!({
+            "name": "test",
+        }));
+}
+
+#[get("/webhooks/youtube")]
+async fn verify_youtube_webhook(youtube_query: web::Query<YoutubeQuery>, app: web::Data<YoutubeApp>) -> impl Responder {
+    let youtube_query = youtube_query.into_inner();
+
+    if youtube_query.verify_token == app.verify_token {
+        return HttpResponse::Ok().body(youtube_query.challenge);
+    }
+
+    HttpResponse::Unauthorized().finish()
+}
+
+#[post("/webhooks/youtube")]
+async fn youtube_webhook(req: HttpRequest, body: Bytes, kafka: web::Data<Arc<FutureProducer>>, app: web::Data<YoutubeApp>) -> impl Responder {
+    if !verify_youtube_signature(req.headers(), &body, app.hmac_secret.as_bytes()) {
+        return HttpResponse::Unauthorized().finish();
+    }
+
+    if let Ok(youtube_feed) = quick_xml::de::from_str::<YoutubeFeed>(std::str::from_utf8(&body).unwrap()) {
+        let producer = kafka.into_inner();
+        
+        for video in youtube_feed.entries {
+            match producer.send(
+                FutureRecord::to("youtube")
+                        .payload(&serde_json::to_string(&EventYoutubeVideo::from(&video)).unwrap())
+                        .key(&video.id),
+            Duration::from_secs(2),
+            ).await {
+                Ok(_) => {
+
+                },
+                Err(error) => {
+                    dbg!(error);
+                }
+            };
+        }
+        
+        HttpResponse::Ok().finish()
+    } else {
+        HttpResponse::NotAcceptable().finish()
+    }
+}
+
+
+#[post("/webhooks/twitch")]
+async fn twitch_webhook(req: HttpRequest, body: Bytes, kafka: web::Data<Arc<FutureProducer>>, twitch: web::Data<TwitchApp>) -> impl Responder {
+    if !verify_twitch_signature(req.headers(), &body, &twitch.hmac_secret) {
+        return HttpResponse::Unauthorized().finish();
+    }
+   
+    if let Ok(twitch_callback) = serde_json::from_slice::<TwitchCallback>(&body) {
+        if let Some(challenge) = twitch_callback.challenge {
+            return HttpResponse::Ok().body(challenge);
+        }
+
+        let producer = kafka.into_inner();
+
+        match dbg!(producer.send(
+            FutureRecord::to("twitch")
+                .payload(&serde_json::to_string(&EventMessage {
+                    event_type: twitch_callback.subscription.event_type,
+                    event: twitch_callback.event.unwrap(),
+                    triggered_at: chrono::Utc::now(),
+                }).unwrap())
+                .key(""),
+            Duration::from_secs(2),
+        ).await) {
+            Ok(_) => {
+
+            },
+            Err(error) => {
+                dbg!(error);
+            }
+        };
+
+        return HttpResponse::Ok().finish();
+    } else {
+        return HttpResponse::InternalServerError().finish();
+    }
+}
+
+#[derive(Debug)]
+struct YoutubeApp {
+    verify_token: Option<String>,
+    hmac_secret: String,
+}
+
+#[derive(Debug)]
+struct TwitchApp {
+    hmac_secret: String,
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
+
+    HttpServer::new(|| {
+        let verify_token = match env::var("YOUTUBE_VERIFY_TOKEN") {
+            Ok(token) => Some(token),
+            Err(_) => None,
+        };
+
+        let producer: FutureProducer = ClientConfig::new()
+            .set("bootstrap.servers", env::var("BROKER_IP").expect("BROKER_IP not in environment"))
+            .set("message.timeout.ms", "2000")
+            .create()
+            .expect("Producer creation error");
+    
+        App::new()
+            .wrap(Logger::default())
+            .data( Arc::new(producer))
+            .data(YoutubeApp {
+                verify_token: verify_token.clone(),
+                hmac_secret: env::var("YOUTUBE_HMAC_SECRET").expect("YOUTUBE_HMAC_SECRET not in environment"),
+            })
+            .data(TwitchApp {
+                hmac_secret: env::var("TWITCH_HMAC_SECRET").expect("TWITCH_HMAC_SECRET not in environment"),
+            })
+            .service(twitch_webhook)
+            .service(verify_youtube_webhook)
+            .service(youtube_webhook)
+            .service(test)
+    })
+        .bind(env::var("LISTEN_ADDRESS").expect("LISTEN_ADDRESS not in environment"))?
+        .run()
+        .await
+}
